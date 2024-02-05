5c747d282b8a3361b0b6838e49d178a29a246ed5
Billy Bagel <itisbilly@gmail.com>
2024-02-05 00:13:33 -0800

    Created 'users/cool' page

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/cool/', function(req, res, next) {
+  res.send('COOL <br> <h1>COOL</h1> <br> COOL');
+})
+
 module.exports = router;
