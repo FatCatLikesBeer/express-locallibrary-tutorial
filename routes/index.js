f09c5f00e890cd57b0dc7e100e7df76582e70100
Billy Bagel <itisbilly@gmail.com>
2024-02-05 01:06:03 -0800

    Finished this section, on to the next one

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'boobis' });
+  res.render('index', { title: 'homepage' });
 });
 
 module.exports = router;
