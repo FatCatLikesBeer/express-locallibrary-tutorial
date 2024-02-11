fedc5149726a18f57a48c55f59c985b177622cd1
Billy Bagel <itisbilly@gmail.com>
2024-02-11 01:07:20 -0800

    Added wiki

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 const mongoDb = require('./db_creds.js');
+const wiki = require('./wiki.js');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -29,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/wiki', wiki);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
