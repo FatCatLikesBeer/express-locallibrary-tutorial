e684de686b7c954e90a6e0a70c834f63a50f16dd
Billy Bagel <itisbilly@gmail.com>
2024-02-11 21:06:34 -0800

    Don't remember what I messed around with but nothing got messed up

--- routes/catalog.js
+++ routes/catalog.js
@@ -96,27 +96,27 @@ router.get("/genres", genre_controller.genre_list);
 /// BOOKINSTANCE ROUTES ///
 
 // GET request for creating BookInstance. NOTE This must come bore route that displays BookInstance (uses id).
-router.get("/book_instance/create", book_instance_controller.bookinstance_create_get);
+router.get("/bookinstance/create", book_instance_controller.bookinstance_create_get);
 
 // POST request for creating BookInstance.
-router.post("/book_instance/create", book_instance_controller.bookinstance_create_post);
+router.post("/bookinstance/create", book_instance_controller.bookinstance_create_post);
 
 // GET request for delete a BookInstance.
-router.get("/book_instance/:id/delete", book_instance_controller.bookinstance_delete_get);
+router.get("/bookinstance/:id/delete", book_instance_controller.bookinstance_delete_get);
 
 // POST request for creating BookInstance.
-router.post("/book_instance/:id/delete", book_instance_controller.bookinstance_delete_post);
+router.post("/bookinstance/:id/delete", book_instance_controller.bookinstance_delete_post);
 
 // GET request for update a BookInstance.
-router.get("/book_instance/:id/update", book_instance_controller.bookinstance_update_get);
+router.get("/bookinstance/:id/update", book_instance_controller.bookinstance_update_get);
 
 // POST request for creating BookInstance.
-router.post("/book_instance/:id/update", book_instance_controller.bookinstance_update_post);
+router.post("/bookinstance/:id/update", book_instance_controller.bookinstance_update_post);
 
 // GET request for one BookInstance.
-router.get("/book_instance/:id", book_instance_controller.bookinstance_detail);
+router.get("/bookinstance/:id", book_instance_controller.bookinstance_detail);
 
 // GET request for a list of all BookInstances.
-router.get("/book_instances", book_instance_controller.bookinstance_list);
+router.get("/bookinstances", book_instance_controller.bookinstance_list);
 
 module.exports = router;
