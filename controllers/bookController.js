d79038ffb4e1e0f49fb5ba4aa5b40eed9af6e1a9
Billy Bagel <itisbilly@gmail.com>
2024-02-12 02:14:39 -0800

    Added a view for 'book_list'

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -35,7 +35,13 @@ exports.index = asyncHandler(async (req, res, next) => {
 
 // Display list of all Books.
 exports.book_list = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Book List');
+  // Display list of all books.
+  const allBooks = await Book.find({}, "title author")
+    .sort({ title: 1 })
+    .populate("author")
+    .exec();
+
+  res.render("book_list", { title: "Book List", book_list: allBooks});
 });
 
 // Display detail page for a specific Book.
