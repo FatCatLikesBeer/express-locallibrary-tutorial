3aef899968a6bc87394ec64c7ed4ec57f8501791
Billy Bagel <itisbilly@gmail.com>
2024-02-13 02:48:14 -0800

    Updated the view in /models/author.js

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
 
 const Schema = mongoose.Schema;
 
@@ -14,7 +15,7 @@ AuthorSchema.virtual("name").get( function () {
   // To avoid errors in cases where an author does not have either a family name or a first name
   // We want to make sure we handle the exception by returning an empty string for that case
   let fullname = "";
-  if (this.first_name && this.last_name) {
+  if (this.first_name && this.family_name) {
     fullname = `${this.family_name}, ${this.first_name}`;
   }
   return fullname;
@@ -24,6 +25,14 @@ AuthorSchema.virtual("name").get( function () {
 AuthorSchema.virtual("url").get(function () {
   // We don't use an arrow function as we'll need the 'this' object
   return `/catalog/author/${this._id}`;
+});
+
+// Virtual for Author's lifetime
+AuthorSchema.virtual("lifetime").get(function () {
+  // We don't use an arrow function as we'll need the 'this' object
+  const bornDay = this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : "Unknown";
+  const deadDay = this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : "";
+  return deadDay ? `${bornDay} - ${deadDay}` : `${bornDay}`;
 });
 
 // Export Model
